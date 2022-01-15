13785630740b6e678e200f9a38a2cf483551d8a0
jkero+xeon <jacques.keroack@gmail.com>
2022-01-15 13:34:38 -0500

    more regles

--- matrice_temps/demarrage_pygcb.py
+++ matrice_temps/demarrage_pygcb.py
@@ -54,20 +54,37 @@ def calcul_equipes(hpers):
 
 # Pour cette semaine on a besoin de (total_hpers/temps_quart) 50/7.5 = 6.6 personnes de quart (de 7.5 heures).
 
-# Exemple plus nombreux avec 1000 hpers. On a 1000/7.5 = 133 personnes de quart. on peut diviser par 5 jours = 26.6 personnes par jour.
+# Exemple plus nombreux avec 1000 hpers. On a 1000/7.5 = 133 quarts pour la semaine; on peut diviser par 5 jours = 26.6 personnes par jour.
 
 # Prenons 27 pour arrondir et on divise par le nb_max par equipe : on a alors 27/4 = 7 equipes de 4 par jour pendant 5 jours.
 
-# donc 7 * 4 * 5 * 7.5  * 4 * 5 * 7.5 = ~1000 heures requises.
+# donc 7 * 4 * 5 * 7.5   = les ~1000 heures/pers requises.
 
-#il faut déterminer le nb de quarts, une autre constante (K3)
+#il faut déterminer le nb de quarts, s'il y a lieu, une autre constante (K3)
 
-# si on a 3 quarts ça fait 2 quarts de 2 et un quart de 3 équipes
+# si on a 3 quarts ça fait 2 quarts de 2 et un quart de 3 équipes, on peut les remplir  jour -> matin -> soir ??
 
 # Réalité : ça prendrait 27 personnes ([1000/7.5h] sans backup) sur le payroll pendant cette période.
 
-# il faut assigner les employés aux équipes selon les règles de gestion des non_dipspo.
+# il faut assigner les employés aux équipes selon les règles de gestion des non_dispo.
 
+#  -- a) choisir les employés soit dans l'ordre d'ancienneté ou alors au hasard ?? (les plus anciens sur les equipes de jour, par exemple)
+#  -- b) pour chaque équipe et cpt < nb_emplo requis,
+#         cpt_eq = 0
+#         si cpt_eq < max
+#           si liste(emp.non_dispo) <> date pour cette affectation (#semaine = dates à comparer pour les 3 types de non-dispo).
+#                ajouter cet employé dans equipe.
+#                 cpt++
+#                 cpt_eq++
+# la rotation des équipes est prédéterminée entre chaque semaine (ou non, c'est fixe, car c'est au moment des validations des non-dispos que la justice s'applique). Selon le nb, il faut permuter logiquement à travers les quarts.
+# ça pourrait être une sequence générée. Logique à étudier: comment permuter si les valeurs changent ? Concept de
+# quart prioritaire (y ajouter des equipes, etc.)
+
+#           s           d           l           m           m           j           v
+#q   matin/jour/soir
+#eq      y/w,x/z      y/w,x/z     c/ab/d     c/ab/d     c/ab/d       c/ab/d      y/w,x/z
+
+# bref, quand un employe est intégré, il faut lui appliquer une grille de non dispos et de quarts, selon la période (on lui assigne un quart + les vacances + les autres non-dispos.).
 
 def select_hpers(conn, sem):
     """
