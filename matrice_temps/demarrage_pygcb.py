1164a8e8693f1c74c2185c61d410ba35e8e1dc0d
jkero+xeon <jacques.keroack@gmail.com>
2022-01-15 12:54:33 -0500

    regles

--- matrice_temps/demarrage_pygcb.py
+++ matrice_temps/demarrage_pygcb.py
@@ -39,6 +39,7 @@ def create_connection(db_file):
 def calcul_equipes(hpers):
     temps_quart = 7.5
     max_emp_par_equipe = 4
+    nb_quarts_par_jour = 3
  # la logique equipes + hpers + quarts est en partie ici.
 
 # 50 heures personnes signifie qu'un travail a besoin de 50 heures * 1 personne
@@ -51,9 +52,21 @@ def calcul_equipes(hpers):
 
 # il faut déterminer le nb max d'employés dans un équipe -- une autre constante (K2).
 
-# pour cette semaine on a besoin de (total_hpers/nb_empl_dispo) equipes
+# Pour cette semaine on a besoin de (total_hpers/temps_quart) 50/7.5 = 6.6 personnes de quart (de 7.5 heures).
 
+# Exemple plus nombreux avec 1000 hpers. On a 1000/7.5 = 133 personnes de quart. on peut diviser par 5 jours = 26.6 personnes par jour.
 
+# Prenons 27 pour arrondir et on divise par le nb_max par equipe : on a alors 27/4 = 7 equipes de 4 par jour pendant 5 jours.
+
+# donc 7 * 4 * 5 * 7.5  * 4 * 5 * 7.5 = ~1000 heures requises.
+
+#il faut déterminer le nb de quarts, une autre constante (K3)
+
+# si on a 3 quarts ça fait 2 quarts de 2 et un quart de 3 équipes
+
+# Réalité : ça prendrait 27 personnes ([1000/7.5h] sans backup) sur le payroll pendant cette période.
+
+# il faut assigner les employés aux équipes selon les règles de gestion des non_dipspo.
 
 
 def select_hpers(conn, sem):
