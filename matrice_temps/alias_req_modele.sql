1a3dcdf06f9eb3eba8ec542dcc6087cee30e618a
jkero+xeon <jacques.keroack@gmail.com>
2022-01-20 12:24:45 -0500

    modele equipes

--- matrice_temps/alias_req_modele.sql
+++ matrice_temps/alias_req_modele.sql
@@ -1,12 +1,11 @@
-select p.hpers, p.heures_par_jour, m.nb_emp_par_eq as nb_max_par_eq, 
-round(p.hpers / p.heures_par_jour,1) as nb_quart_indivi, m.nb_eq,
-round(round(p.hpers / p.heures_par_jour,1)/m.nb_emp_par_eq,1) 
-as nb_quarts_eq, round(round(p.hpers / p.heures_par_jour,1) /(m.nb_emp_par_eq *
-m.nb_creneau_quart_jour * m.nb_eq_creneau),1) as jours  
+select p.hpers, p.heures_par_jour, round(p.hpers / p.heures_par_jour,1) as presences, m.nb_eq,m.nb_emp_par_eq as nb_par_eq, 
+round(round(p.hpers *1.0 / p.heures_par_jour,1)/m.nb_emp_par_eq,1) 
+as nb_quarts_eq, m.nb_eq_par_creneau, m.nb_creneau_disp ,round(round(cast(p.hpers as float) / p.heures_par_jour,2) /(m.nb_emp_par_eq *
+m.nb_eq_par_creneau * m.nb_creneau_disp),2) as jours, m.nom  
 
 from previsions_hpers as p
 
 inner join modele_assignation_hebdo as m
 
-where m.id = 1 
---on p.modele = m.id 
+--where m.id = 2
+on p.modele = m.id 
