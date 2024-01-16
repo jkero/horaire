ce0688722c2bfa273900436c2200ebb08d8aece4
jkero+xeon <jacques.keroack@gmail.com>
2024-01-16 16:23:03 -0500

    revue 2024 mariadb tables

--- matrice_temps/refonte_2024/tests/sql_create_tables.sql
+++ matrice_temps/refonte_2024/tests/sql_create_tables.sql
@@ -25,3 +25,29 @@ insert into non_dispo(creneaux, FK_emp) values ('2025-03-18 08:00@2025-03-28 17:
 
 select id,nom, prenom, creneaux from employe right join non_dispo on employe.id = non_dispo.emp_id;
 
+create table modele_affectations (id smallint unsigned not null auto_increment primary key,
+nom varchar(240) not null,
+description varchar(240),
+nb_quarts int not null,
+duree_quart decimal(2,1) unsigned zerofill not null,
+nb_equipes_par_quart int,
+nb_employe_par_equipe int
+) engine = InnoDB;
+
+insert into modele_affectations(nom, description, nb_quarts, duree_quart, nb_equipes_par_quart, nb_employe_par_equipe) values ('minimal', 'Pas ou peu de travail', 1, 7.5, 1,2);
+
+create table previsions_par_semaine (id smallint unsigned not null auto_increment primary key,
+annee int,
+num_semaine int,
+prevision_pers_h decimal(6,1) unsigned zerofill not null,
+modele_id smallint unsigned not null,
+constraint `FK_modele`
+foreign key (modele_id) references modele_affectations(id)
+on delete cascade
+) engine = InnoDB;
+
+insert into previsions_par_semaine(annee,num_semaine,prevision_pers_h, modele_id) values (2025,52,30,1);
+
+select * from modele_affectations as p right join previsions_par_semaine as r on r.id = p.id;
+
+
