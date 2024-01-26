5fc09570913e17172fbaadabc002ee4a12fdc359
jkero+xeon <jacques.keroack@gmail.com>
2024-01-26 12:36:08 -0500

    dico_semaine produit

--- matrice_temps/refonte_2024/tests/util_compose_equipes.py
+++ matrice_temps/refonte_2024/tests/util_compose_equipes.py
@@ -15,7 +15,7 @@ class CompositionEquipes:
         nb_equipes_par_q = m.nb_equipes_par_q
         nb_quarts = m.nb_quarts
         nb_leads = nb_quarts * nb_equipes_par_q
-        queryLeads = "select * from employe where anciennete > 55 and niveau >= 3 order by niveau desc, anciennete desc"
+        queryLeads = "select id, num_emp, nom, prenom from employe where anciennete > 55 and niveau >= 3 order by niveau desc, anciennete desc"
         # # combien d'équipes par jour = nb_quarts * nb_eq_par_quart
         jkcur.execute(queryLeads)
         l_all_leads = jkcur.fetchall()
@@ -28,7 +28,7 @@ class CompositionEquipes:
         nb_equipes_par_q = m.nb_equipes_par_q
         nb_quarts = m.nb_quarts
         nb_under_leads = (nb_quarts * nb_equipes_par_q) - 1
-        queryUnderLeads = "select * from employe where anciennete <= 55 and niveau < 3 order by niveau desc, anciennete desc"
+        queryUnderLeads = "select id, num_emp, nom, prenom from employe where  niveau < 3 order by anciennete desc, niveau desc"
         # # combien d'équipes par jour = nb_quarts * nb_eq_par_quart
         jkcur.execute(queryUnderLeads)
         l_all_under_leads = jkcur.fetchall()
@@ -45,7 +45,7 @@ class CompositionEquipes:
         jkcur.execute(queryNonDispo)
         liste_non_dispos = list(jkcur.fetchall())
 
-        return liste_non_dispos
+        return list(liste_non_dispos)
 
     @staticmethod
     def get_emp_dispo(premier_jour_sem, an, semaine):
@@ -66,65 +66,53 @@ class CompositionEquipes:
         liste_jours_semaine = LaSemaine.renseigne_jours_semaine(premier_jour_sem,an,semaine)# \\todo hard code here for tests ;1st day must be 0 (Monday) to 6 (Sunday)
         print (liste_jours_semaine)
 
+        dict_equipes_semaine = {}
+
         for jour in liste_jours_semaine:
+            dict__jour = {}# une version par jour (contient les dicos equipes)
+
+            # // ici je réinitialise les listes des employés diponibles (vérif par jour)
             liste_all_lead_pop = liste_all_leads.copy()
             liste_all_under_leads_pop = liste_all_under_leads.copy()
+
+            # // ici: 2 boucles pour vérifier les non_dispos et valider les listes
             for pot_lead in liste_all_lead_pop:
-                #print(pot_lead[0])
                 for non_dispo in liste_all_non_dispos:
-#                    print(non_dispo[0])
                     if Check_non_dispo.is_not_dispo(non_dispo[3],jour[1]):
                         if(pot_lead[0] == non_dispo[0]):
-                            print (" %s  %s" % (str(jour[1]),str(pot_lead[2])))
                             liste_all_lead_pop.pop(liste_all_lead_pop.index(pot_lead))
 
             for pot_under_lead in liste_all_under_leads_pop:
-                # print(pot_lead[0])
                 for non_dispo in liste_all_non_dispos:
-                    #                    print(non_dispo[0])
                     if Check_non_dispo.is_not_dispo(non_dispo[3], jour[1]):
                         if (pot_under_lead[0] == non_dispo[0]):
-                            print(" %s  %s" % (str(jour[1]), str(pot_under_lead[2])))
                             liste_all_under_leads_pop.pop(liste_all_under_leads_pop.index(pot_under_lead))
 
+            dict_equipe = {} # une version par jour
 
+            # // ici je renseigne le dico equipe (chaque équipe) avec les leaders disponibles
+            for i in range(modele_previsions.nb_quarts * modele_previsions.nb_equipes_par_q):# = le nb equipes total
+                dict_equipe[liste_all_lead_pop[i][2]] = list(liste_all_lead_pop[i])
+
+
+            # // ici je récupère ce qu'il faut d'employés disponibles (triés par ancienneté, niveau) et les ajoute aux dicos equipes
+            for i in range(((modele_previsions.nb_emplo_par_eq - 1))):
+                #print(i)
+                for j in dict_equipe:
+                    dict_equipe[j].append(list(liste_all_under_leads_pop.pop(0)))
+
+
+            # // finalement la semaine est structurée dans un dictionnaire de jours et d'équipes
+            dict_equipes_semaine[jour[1][:10]] = dict_equipe
+
+        #vérif la semaine
+        for j in dict_equipes_semaine:
+            print(j)
+            for v in dict_equipes_semaine[j].values():
+                print(v)
+            # for e in j:
+            #     print("\n\t %s" % str(e))
         #ltot = liste_all_lead_pop
-
-
-        jkcur = CompositionEquipes.connection.conn.cursor()
-        # queryLeads = "select * from employe where anciennete > 55 and niveau >= 3 order by niveau desc, anciennete desc"
-        # # combien d'équipes par jour = nb_quarts * nb_eq_par_quart
-        # nb_leads = nb_quarts * nb_equipes_par_q
-        # # print("nb équipes par jour = %d" % (nb_leads))
-        # jkcur.execute(queryLeads)
-        # # jkcur.fetchall(int(nb_leads))
-        #
-        # for row in jkcur.fetchmany(int(nb_leads)):
-        # # print(row)
-        # list_e = list([row[1], row[2], row[3]])
-        # liste_leads.append(list_e)
-        # # dict_leads["Team " + row[2]] = list([list_e])
-        #
-        # # print("\nTEST 3 ---- nb leads = tot eq du modele ? (%d = %d)" % (len(liste_leads), nb_leads))
-        #
-        # # creer liste equipes
-        #
-        # liste_emp = []
-        # # print("liste employes requis = %d" % (int(nb_leads) * int(nb_emplo_par_eq - 1)))
-        # # //todo l'algo écarte tous les autres leads des équipes (comme employe ordinaire), ajuster
-        # queryNoLeads = "select num_emp, nom, prenom,id from employe where anciennete <= 55 and niveau < 3 order by niveau desc, anciennete desc"
-        # jkcur.execute(queryNoLeads)
-        # # premiere passe des no-leads: toute la liste des dispos
-        #
-        # les_emp_orig = list(jkcur.fetchall())
-        # les_emp_to_pop = les_emp_orig
-        # # on elimine les-non-dispos tout en renseignant la liste avec le nb (nb_emplo_par_eq - 1) requis
-        #
-        # queryNonDispo = "select emp_id, nom, prenom, creneaux from employe right join non_dispo on employe.id = non_dispo.emp_id order by creneaux"
-        # jkcur2 = la_conn.conn.cursor()
-        # jkcur2.execute(queryNonDispo)
-        # liste_non_dispos = jkcur2.fetchall()
-
 
 if __name__ == '__main__':
     CompositionEquipes.get_emp_dispo(0,2024,6)
