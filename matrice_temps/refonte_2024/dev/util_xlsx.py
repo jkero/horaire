32d95176c7a411cea5275a3f7947102d336d4674
jkero+xeon <jacques.keroack@gmail.com>
2024-01-30 12:28:04 -0500

    trouve erreurs ventilation heures

--- matrice_temps/refonte_2024/dev/util_xlsx.py
+++ matrice_temps/refonte_2024/dev/util_xlsx.py
@@ -44,148 +44,64 @@ class Prod_chiffrier:
         cell_format_red_small = wb.add_format({'bold': False, 'font_color': 'red', 'font_size': '13','align': 'right','valign':'vcenter' })
         cell_format_noir = wb.add_format({'bold': True, 'font_color': 'black','text_wrap':'true','align':'right','valign':'vcenter'})
         cell_format_bleu = wb.add_format({'bold': False, 'font_color': 'blue', 'font_size': '15','text_wrap': 'true', 'align': 'center', 'valign': 'vcenter'})
-        cell_format_vert = wb.add_format({'italic': True, 'bold': False, 'font_color': '#337722', 'font_size': '13','text_wrap': 'true', 'align': 'center', 'valign': 'vcenter'})
-
+        cell_format_vert = wb.add_format({'italic': True, 'bold': False, 'font_color': '#337722', 'font_size': '13','text_wrap': 'true', 'align': 'right', 'valign': 'vcenter'})
+        cell_format_ardoise = wb.add_format({'italic': True, 'bold': False, 'font_color': '#2F4F4F', 'font_size': '12','text_wrap': 'true', 'align': 'center', 'valign': 'vcenter'})
+        une_string = "Semaine # " + str(CompositionEquipes.modele.prev_num_sem) \
+            + ", Nb quarts: " + str(CompositionEquipes.modele.nb_quarts) \
+            + ", Nb équipes par quart: " + str(CompositionEquipes.modele.nb_equipes_par_q)  \
+            + ", Nb employés par équipes: " \
+            + str(CompositionEquipes.modele.nb_emplo_par_eq) + " ,Heures totales: " \
+            + str(CompositionEquipes.modele.prev_heures_sem) + ", Modele : " + str(CompositionEquipes.modele.id_mod)
+        ws.merge_range(0, 2, 0, 5, '')
+        print(une_string)
+        ws.write(0,2, str(une_string), cell_format_ardoise)
+        col_quart = col_date = 1 #repères pour excel
+        col_nom_equipiers = 3
+        col_chef_equipe = 2
         ws.set_column('A:A', 20)
         ws.write(1,0, "Émis le :")
         ws.set_row(2, 20)
         ws.write(2, 0, Prod_chiffrier.aujourd.strftime('%y-%m-%d %H:%M'), cell_format_bleu)
         ws.set_row(0, 44)
         ws.write('A1', 'Equipes', cell_format_red_small)
-        col = 2
-        row = 3
-        s_jour_date = CompositionEquipes.semaine
+        debut_donnees = row = 3
         s_equipe_jour = Prod_chiffrier.dict_semaine
-        #print(str(str(s_equipe_jour)))
+
         for date_jour in s_equipe_jour.keys():
-            col = 1 # dates
+            eq_pop = s_equipe_jour[date_jour]
+            col = col_date # dates
             ws.set_column(1, 1, 22)
             ws.write_string(row, col, str(date_jour), cell_format_red_small)
             row = row + 1
-            for team in s_equipe_jour[date_jour]: #nom_eq (chef)
-                col = 1
-                ws.write_string(row, col, str(team), cell_format_vert)
-                col = 2
-                for equipiers in s_equipe_jour[date_jour][team]:
-                    ws.set_column(col, col, 29)
-                    a = str( " %s, %s (%s)" % (equipiers[2],equipiers[3],equipiers[1]))
-                    ws.write_string(row, col, a, cell_format_noir)
-                    col = col + 1
-                #
-                col = 2
-                row = row + 1
+            les_chefs = list(eq_pop) # pas plus bas : doit être poppé
+            for q in range(CompositionEquipes.modele.nb_quarts):
+                row = row +1
+                #print("quart %s" % str(q+1))
+                col = col_quart
+                ws.write_string(row, col, "quart " + str(q + 1), cell_format_ardoise)
+                for eq_p_q in range(CompositionEquipes.modele.nb_equipes_par_q):
+                    chef = les_chefs.pop(0)
+                    col = col_chef_equipe
+                    ws.write_string(row, col, str(chef), cell_format_vert)
+                    col = col_nom_equipiers
+                    for equipiers in s_equipe_jour[date_jour][chef]:
+                        ws.set_column(col, col, 29)
+                        a = str(" %s, %s (%s)" % (equipiers[2], equipiers[3], equipiers[1]))
+                        ws.write_string(row, col, a, cell_format_noir)
+                        col = col + 1
+                    row = row + 1
+            row = row + 1    #print(eq_p_q)
+
 
     @staticmethod
     def onglet_modele(wb):
         ws = wb.add_worksheet('Modèle')
         le_modele = CompositionEquipes.modele
         cell_format_red = wb.add_format({'bold': True, 'font_color': 'red', 'font_size': '20', 'align': 'center', 'valign': 'vcenter'})
-        une_string = "Semaine # " + str(le_modele.prev_num_sem) + ", Nb quarts: " + str(le_modele.nb_quarts) + ", Nb équipes par quart: " + str(le_modele.nb_equipes_par_q) +", Nb employés par équipes: " + str(le_modele.nb_emplo_par_eq)
+        une_string = "Semaine # " + str(le_modele.prev_num_sem) + ", Nb quarts: " + str(le_modele.nb_quarts) + ", Nb équipes par quart: " + str(le_modele.nb_equipes_par_q) + ", Nb employés par équipes: " + str(le_modele.nb_emplo_par_eq) + "Heures totales : " + str(le_modele.prev_heures_sem)
         #cell_format = wb.add_format({'bold': True, 'font_color': 'red'})
-        ws.merge_range(1,1,3,15, '')
+        ws.merge_range(1,1,3,25, '')
         ws.write_string(1,1, str(une_string),cell_format_red)
-
-        #################
-        #         for j in range(0, len(self.calendrier_equipes[keys][indx])):
-        #             ws.write(row,col,str(self.calendrier_equipes[keys][indx][0][0]),cell_format_noir) #nom eq
-        #             for r in range(0, len(self.calendrier_equipes[keys][indx][1])): #nb eq
-        #                 ws.write(row, col+ r + 1 ,str(self.calendrier_equipes[keys][indx][1][r]))
-        #     ws.set_column(row, col, 15)
-        # row = row + self.config_modele[0][5]
-        # date_prod = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-        # ws.write_string(row, col+1, "Émis le " + date_prod, cell_format_red)
-        #
-        # # --------------autre feuille excel -------------------------------------------------------------------------
-        # row = 1
-        #
-        # ws2 = wb.add_ws('calendrier')
-        # colo = 0
-        #
-        # ws2.write_string(row, colo, 'Semaine ' + str(self.config_modele[0][0]), cell_format_red) # colo passe pas ?
-        # colo = colo + 1
-        #
-        # print(str(self.config_modele))
-        # mod_hpers = self.config_modele[0][1]
-        # nb_cren = self.config_modele[0][8]
-        # nb_jour_sem = len(self.les_jours)
-        # nb_eq = self.config_modele[0][4]
-        # eq_par_cren = int(self.config_modele[0][7]+.5)
-        # calc_nb_quarts_requis= round(self.config_modele[0][6])
-        # empl_par_eq = self.config_modele[0][5]
-        # print("date de réf. :" + str(self.auj))
-        # print("sem : " + str(self.config_modele[0][0]))
-        # print("Modele : " + str(self.config_modele[0][10]) + " h-pers = " + str(mod_hpers))
-        # print("\t Calcul présences totales d'équipes: " + str(calc_nb_quarts_requis))
-        # print("\t Calcul présences individuelles: " + str(self.config_modele[0][3]))
-        # print("\t Créneaux par jour: " + str(nb_cren))
-        # print("\t Equipes par créneau: " + str(eq_par_cren))
-        # print("\t Nombre d'équipes: " + str(nb_eq))
-        # print("\t Empl. par éq.: " + str(empl_par_eq))
-        # print("\t Durée quart: " + str(self.config_modele[0][2]))
-        #
-        # # ligne1
-        # eqs = self.les_cles
-        # eqs2 = eqs[:]
-        # tot_affec = 0
-        # tot_h_affec = 0
-        # ws2.write(row, colo, 'Horaire')
-        #
-        # for i in range(1, 4):
-        #     ws2.write(row, colo + i, 'Q'+ str(i))
-        #
-        # for ix in range(0, len(self.les_jours)):
-        #     row = row + 1
-        #     ws2.write(row, colo, str(self.les_jours[ix][0]) + "\n" + str(self.les_jours[ix][1]), cell_format_noir)
-        #     ws2.set_column(row, colo, 15)
-        # row = row - 6
-        # pop_string_eq = ""
-        #
-        # eq_courante = ''
-        # for jour in range(0, nb_jour_sem):                  #--- Pour chaque jour
-        #     cpt_cren = 0                                    # suivi de la position pour grille
-        #     for cren in range(1, nb_cren + 1):              #--- Pour chaque creneau
-        #         cpt_cren = cpt_cren + 1
-        #         colo = colo +1# cren_dispo
-        #         pop_string_eq = ""                          #chaine pour concat equipes dans un seul creneau (c=1 epc>1)
-        #         for eq in range(0, eq_par_cren):            # Pour chaque equipe
-        #             if tot_affec <  calc_nb_quarts_requis: # and tot_h_affec < mod_hpers:  # on interrompt si le nb equipes arrive au nb_calculé
-        #                 if len(eqs) > 0:
-        #                     eq_courante = eqs.pop(0)  # liste eq non vide on affecte et retire une equipe
-        #                     pop_string_eq = pop_string_eq + " " + eq_courante
-        #                     ws2.write(row, colo, pop_string_eq.strip())
-        #                     tot_affec = tot_affec + 1
-        #                 else:
-        #                     if len(eqs2)/eq_par_cren >=  nb_cren:
-        #                         eqs = eqs2[:]
-        #                         eq_courante = eqs.pop(0)
-        #                         pop_string_eq = pop_string_eq + " " + eq_courante
-        #                         tot_affec = tot_affec + 1
-        #                         ws2.write_string(row, colo, pop_string_eq.strip())
-        #                     else:
-        #                         eqs = eqs2[:]
-        #                         break
-        #
-        #             else:
-        #                 break
-        #     colo = colo - cpt_cren
-        #     row = row + 1
-        #
-        # colo = colo - 1
-        # ws2.write_string(row + 2, colo, "Modele : " + str(self.config_modele[0][10]) + " h-pers = " + str(self.config_modele[0][1]), cell_format_red)
-        # row = row + 3
-        # la_longue_string_modele = ""
-        # la_longue_string_modele = la_longue_string_modele + " Calcul pr. équipes: " + str(calc_nb_quarts_requis) + "\n"
-        # la_longue_string_modele = la_longue_string_modele + " Calcul prés individuelles: " + str(self.config_modele[0][3]) + "\n"
-        # la_longue_string_modele = la_longue_string_modele + " Créneaux par jour: " + str(nb_cren) + "\n"
-        # la_longue_string_modele = la_longue_string_modele + " Equipes par créneau: " + str(eq_par_cren) + "\n"
-        # la_longue_string_modele = la_longue_string_modele + " Nombre d'équipes: " + str(nb_eq) + "\n"
-        # la_longue_string_modele = la_longue_string_modele + " Empl. par éq.: " + str(self.config_modele[0][5]) + "\n"
-        # la_longue_string_modele = la_longue_string_modele + " Durée quart.: " + str(self.config_modele[0][2]) + "\n"
-        # ws2.write_string(row, colo, la_longue_string_modele, cell_format_noir)
-        # ws2.set_column(row, colo, 23)
-        # row = row + 2
-        # date_prod = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-        # ws2.write_string(row, colo+1, "Émis le " + date_prod, cell_format_red)
         wb.close()
 
 Prod_chiffrier.initialise() #juste pourverifier quelques lignes sinon ça plante pour le moment
