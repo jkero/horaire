95b0dbf172981dc514a405c1c7b1450a865b36fb
jkero+xeon <jacques.keroack@gmail.com>
2024-02-06 11:13:12 -0500

    quelques documentations avec autodoc.

--- matrice_temps/refonte_2024/tests/test_xlsx.py
+++ matrice_temps/refonte_2024/tests/test_xlsx.py
@@ -3,6 +3,9 @@ from datetime import timedelta, datetime
 import locale
 import calendar
 import sys, os, unittest
+
+import xlsxwriter.exceptions
+
 from matrice_temps.refonte_2024.dev import util_xlsx
 
 class TestProd_xlsx(unittest.TestCase):
@@ -12,19 +15,25 @@ class TestProd_xlsx(unittest.TestCase):
     """
 
     def test_initialise(self):
-        horaire = util_xlsx.Prod_chiffrier
-        zefile = os.path.join('C:', 'sphynx_repo', 'sphinx_repo', 'horaire', 'matrice_temps', 'refonte_2024', 'dev', 'builds_xlsx')
-        print("zefile :" + zefile)
-        props = horaire.initialise(zefile, 0, 2024, 44)
-        print('Aujourd\'hui:')
-        print(str(horaire.aujourd))
-        print('La semaine selon ces paramètres: année: %s  num semaine: %s, prem_jour: %d (round trip mariadb)' % ( horaire.annee, horaire.semaine, horaire.prem_jour_sem))
-        print(str(horaire.date_sem_ref))
-        #       xd = datetime.strptime(d, '%Y-%m-%d %H:%M')
-        print("Start of week:", datetime.strptime(str(horaire.date_sem_ref[0][1]),"%Y-%m-%d %H:%M"))
-        print(datetime.strptime(str(horaire.date_sem_ref[0][1]),"%Y-%m-%d %H:%M").weekday())
-        # print("_______________________________________________")
-        # print(str(horaire.dict_semaine))
+        try:
+            horaire = util_xlsx.Prod_chiffrier
+            #zefile = os.path.join('C:\', 'sphynx_repo', 'sphinx_repo', 'horaire', 'matrice_temps', 'refonte_2024', 'dev', 'builds_xlsx')
+            zefile = os.path.join('/','home','jack','python_projets','horaire','horaire','matrice_temps','refonte_2024','dev','builds_xlsx')
+            print("zefile :" + zefile)
+            props = horaire.initialise(zefile, 0, 2024, 44)
+            print('Aujourd\'hui:')
+            print(str(horaire.aujourd))
+            print('La semaine selon ces paramètres: année: %s  num semaine: %s, prem_jour: %d (round trip mariadb)' % ( horaire.annee, horaire.semaine, horaire.prem_jour_sem))
+            print(str(horaire.date_sem_ref))
+            #       xd = datetime.strptime(d, '%Y-%m-%d %H:%M')
+            print("Start of week:", datetime.strptime(str(horaire.date_sem_ref[0][1]),"%Y-%m-%d %H:%M"))
+            print(datetime.strptime(str(horaire.date_sem_ref[0][1]),"%Y-%m-%d %H:%M").weekday())
+            # print("_______________________________________________")
+            # print(str(horaire.dict_semaine))
+        except  xlsxwriter.exceptions.FileCreateError as e:
+            print(e)
+        finally:
+            horaire.wb.close()
 
 
 
